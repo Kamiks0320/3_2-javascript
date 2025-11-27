56a0ef078ccf229231071d74f8cc23f8f9e45267
Kamil <wegrzynk322@gmail.com>
2025-11-27 19:09:06 +0100

    Com3_2.1

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-const token = "LnnkZFiQykYubPfxKXMjLBqHZQbVnLTB"
-
 document.getElementById("searchButton").addEventListener("click", function()
 {
-    fetch("https://corsproxy.io/?https://www.ncei.noaa.gov/cdo-web/api/v2/stations?limit=5", 
-        {
-            headers:
-            {
-                token: token
-            }
-        })
+    fetch("http://localhost:3000/stations")
         .then(response =>
         {
             if(!response.ok) throw new Error("Błąd:" + response.status);
@@ -23,7 +15,7 @@ document.getElementById("searchButton").addEventListener("click", function()
 
             data.results.forEach(station =>
             {
-                const tr = document.getElementById("tr");
+                const tr = document.createElement("tr");
 
                 tr.innerHTML =
                 `
